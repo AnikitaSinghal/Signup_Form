278291c50a55bb227f6e4d28ac216cfe435b871d
Anikita Singhal <92014545+AnikitaSinghal@users.noreply.github.com>
2022-02-23 19:33:38 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./styles.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LandingPage from "./components/LandingPage";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import Welcome from "./components/Welcome";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/welcome" element={<Welcome />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
---
+++ src/components/Signup.jsx
@@ -0,0 +1,10 @@
+function SignUp() {
+  return (
+    <form>
+      <input type="text" placeholder="email" />
+      <input type="password" placeholder="password" />
+      <button type="submit">Sign Up</button>
+    </form>
+  );
+}
+export default SignUp;
